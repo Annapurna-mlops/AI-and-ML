1e2d24bedb882de6a29627eb4962751406612ea1
Annapurna <annapurna.mlops@gmail.com>
2024-06-10 21:40:58 -0500

    numpy and with pyspark env

--- python/abc/iot/message.py
+++ python/abc/iot/message.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum, auto
 
-
+"""
+The enum.auto() method is only available in Python 3.4 and later.
+The enum.auto() method can be used with any type of enum, including IntEnum and Flag.
+The enum.auto() method will always assign unique values to enum members.
+The enum.auto() method can be used to generate values for enum members that are defined in a separate module.
+Overall, the enum.auto() method is a convenient and powerful tool for generating values for enum members.
+"""
 class MessageType(Enum):
     SWITCH_ON = auto()
     SWITCH_OFF = auto()
@@ -10,6 +16,9 @@ class MessageType(Enum):
     OPEN = auto()
     CLOSE = auto()
 
+    """The @dataclass decorator examines the class to find field s.
+    A field is defined as a class variable that has a type annotation.
+    """
 
 @dataclass
 class Message:
